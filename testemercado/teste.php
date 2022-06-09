b88613c2b1564a874b0c6dde307cb4fe1b584192
João Paulo de Lima Novaes <94202128+Jplnovaes@users.noreply.github.com>
2022-06-09 20:59:47 -0300

    Add files via upload

---
+++ testemercado/teste.php
@@ -0,0 +1,32 @@
+<?php
+    require_once 'vendor/autoload.php';
+
+    MercadoPago\SDK::setAccessToken("APP_USR-7510061749105494-052513-60821d5fb2aab686cebdbb7de8b721b0-236485965");
+
+    $payment = new MercadoPago\Payment();
+    $payment->transaction_amount = (float)$_POST['transactionAmount'];
+    $payment->token = $_POST['token'];
+    $payment->description = $_POST['description'];
+    $payment->installments = (int)$_POST['installments'];
+    $payment->payment_method_id = $_POST['paymentMethodId'];
+    $payment->issuer_id = (int)$_POST['issuer'];
+
+    $payer = new MercadoPago\Payer();
+    $payer->email = $_POST['cardholderEmail'];
+    $payer->identification = array(
+        "type" => $_POST['identificationType'],
+        "number" => $_POST['identificationNumber']
+    );
+    $payer->first_name = $_POST['cardholderName'];
+    $payment->payer = $payer;
+
+    $payment->save();
+
+    $response = array(
+        'status' => $payment->status,
+        'status_detail' => $payment->status_detail,
+        'id' => $payment->id
+    );
+    echo json_encode($response);
+
+?>
